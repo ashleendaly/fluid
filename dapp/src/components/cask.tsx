fa7ba640bb13bbafa8cfdcde3868fb56a065cab1
KC-tech-dotcom <keiraemmacrafts@gmail.com>
2024-04-13 19:45:14 +0100

    changes on cask file

--- dapp/src/components/cask.tsx
+++ dapp/src/components/cask.tsx
@@ -8,10 +8,18 @@ import { useContext } from "react";
 export default function Cask() {
     const [isBuyClicked, setIsBuyClicked] = useState(false);
     const [isSellClicked, setIsSellClicked] = useState(false);
+    // I have thse states and I want to set them to certain values using a fetch call to the backend
+    const [caskData, setCaskData] = useState({  
+        caskID: 0,     
+        tokensInLiquidityPool: 0,
+        ageOfCask: 0,
+        priceOfToken: 0,
+        amountOfTokensOwned: 0
+    });
 
   return (
     <div className="">
-        <h2>Cask Id: {}</h2>
+        <h2>Cask Id: {caskData.caskID}</h2>
         <p>Amount of tokens in liquidity pool that are present: {caskData.tokensInLiquidityPool}</p>
         <p>Age of cask: {caskData.ageOfCask}</p>
         <p>Price of token: {caskData.priceOfToken}</p>
@@ -27,8 +35,7 @@ export default function Cask() {
             if amount you enterd is an amount that is not sth you have, it gives a warning " you don't have this amount"
             */}
             </input>
-        )}
-                    
+        )}              
     </div>
   );
 }
