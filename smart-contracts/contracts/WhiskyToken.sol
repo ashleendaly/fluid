86cef501479c0fdfb03dd7ff4d1b48cca0b4343a
KC-tech-dotcom <keiraemmacrafts@gmail.com>
2024-04-13 16:40:48 +0100

    mint batch merge fix

--- smart-contracts/contracts/WhiskyToken.sol
+++ smart-contracts/contracts/WhiskyToken.sol
@@ -4,7 +4,6 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 
 contract CaskTokenContract is ERC1155, Ownable {
     uint256[] private allCasksCreated;
@@ -16,20 +15,9 @@ contract CaskTokenContract is ERC1155, Ownable {
         bytes memory defaultData = hex"12";
         uint256 amount = 1000;
         _mint(accountTokenBeingSentTo, id, amount, defaultData);
-        // call liquidity pool factory
+        // call liquidity pool factory to create a new liquidity pool for this token type.
     }
     
-    function mintBatch(address to, uint256[] memory ids, bytes memory data) public {
-        // get the size of the ids list
-        uint256 numberOfCasksinBatch = ids.length;
-        // amounts = size of the id list * 1000
-        uint256[] memory amounts = new uint256[] (numberOfCasksinBatch);
-        for (uint256 i = 0; i < numberOfCasksinBatch; i++){
-            amounts[i] = 1000;
-        }
-        _mintBatch(to, ids, amounts, data);
-        }
-
     function generatePortfolio(address owner) public view returns (uint256[] memory) {
         // get the number of cask token types that this person has
         uint256 caskTokenCount = 0;
