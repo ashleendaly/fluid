c4e4d66641ead6202f2869ebe515f6bdb82f17f2
ashleendaly <Ashleen.Daly@res-group.com>
2024-04-13 16:56:11 +0100

    change url to casks

--- dapp/src/app/portfolio/page.tsx
+++ dapp/src/app/portfolio/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 type Portfolio = {
   caskId: number;
   balance: number;
@@ -10,14 +12,38 @@ const testData: Portfolio[] = [
 
 const Portfolio = () => {
   return (
-    <div className="p-5">
-      {testData.map((cask) => {
-        return (
-          <div className="border rounded p-3" key={cask.caskId}>
-            {cask.caskId}
-          </div>
-        );
-      })}
+    <div className="p-5 flex flex-col gap-2">
+      <table>
+        <thead>
+          <tr>
+            <th>Asset</th>
+            <th>Price</th>
+            <th>Invested</th>
+            <th>Units</th>
+            <th>Trade</th>
+          </tr>
+        </thead>
+        <tbody>
+          {testData.map((cask) => {
+            return (
+              <tr className="border p-3" key={cask.caskId}>
+                <td>{cask.caskId}</td>
+                <td>-</td>
+                <td>-</td>
+                <td>{cask.balance}</td>
+                <td>
+                  <Link
+                    className="bg-green-400 p-1 px-2 rounded"
+                    href={`/${cask.caskId}`}
+                  >
+                    Trade
+                  </Link>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 };
