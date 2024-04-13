d3b0be504040c830fad0eb161517e16360bb7ff5
KC-tech-dotcom <keiraemmacrafts@gmail.com>
2024-04-13 16:27:39 +0100

    keira's on deployment scripts

--- smart-contracts/hardhat.config.ts
+++ smart-contracts/hardhat.config.ts
@@ -2,7 +2,16 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
+  defaultNetwork: "sepolia",
+  networks: {
+    hardhat: {},
+    sepolia: {
+      url: process.env.SEPOLIA_URL,
+      accounts: [process.env.PRIVATE_KEY || ""],
+    },
+  },
   solidity: "0.8.24",
+
 };
 
 export default config;
---
+++ smart-contracts/scripts/deploy/whiskyToken.ts
@@ -0,0 +1,13 @@
+async function deployWhiskyToken() {
+    try {
+      const WhiskyToken = await require("hardhat").ethers.getContractFactory("Whisky Token");
+  
+      const contract = await WhiskyToken.deploy();
+      console.log("Contract deployed at:", contract.target);
+    } catch (error) {
+      console.error(error);
+      process.exit(1);
+    }
+  }
+  
+  deployWhiskyToken();
