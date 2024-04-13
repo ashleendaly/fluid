49cee1c916c24913ca530eac7ecd9c5aaa89ad38
ashleendaly <Ashleen.Daly@res-group.com>
2024-04-13 19:23:09 +0100

    basic table working

--- dapp/src/app/portfolio/page.tsx
+++ dapp/src/app/portfolio/page.tsx
@@ -1,4 +1,12 @@
 import Link from "next/link";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 type Portfolio = {
   caskId: number;
@@ -13,37 +21,37 @@ const testData: Portfolio[] = [
 const Portfolio = () => {
   return (
     <div className="p-5 flex flex-col gap-2">
-      <table>
-        <thead>
-          <tr>
-            <th>Asset</th>
-            <th>Price</th>
-            <th>Invested</th>
-            <th>Units</th>
-            <th>Trade</th>
-          </tr>
-        </thead>
-        <tbody>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead className="w-[100px]">Asset</TableHead>
+            <TableHead>Price</TableHead>
+            <TableHead>Invested</TableHead>
+            <TableHead>Units</TableHead>
+            <TableHead className="text-right">Trade</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
           {testData.map((cask) => {
             return (
-              <tr className="border p-3" key={cask.caskId}>
-                <td>{cask.caskId}</td>
-                <td>-</td>
-                <td>-</td>
-                <td>{cask.balance}</td>
-                <td>
+              <TableRow className="border p-3" key={cask.caskId}>
+                <TableCell>{cask.caskId}</TableCell>
+                <TableCell>-</TableCell>
+                <TableCell>-</TableCell>
+                <TableCell>{cask.balance}</TableCell>
+                <TableCell>
                   <Link
                     className="bg-green-400 p-1 px-2 rounded"
                     href={`/${cask.caskId}`}
                   >
                     Trade
                   </Link>
-                </td>
-              </tr>
+                </TableCell>
+              </TableRow>
             );
           })}
-        </tbody>
-      </table>
+        </TableBody>
+      </Table>
     </div>
   );
 };
--- dapp/src/smart-contracts.ts
+++ dapp/src/smart-contracts.ts
@@ -1,2 +1,3 @@
 export const whiskyTokenAddress = "0xD5Cb53f53A2920C4e5e62908aEE103f702a0BDE5";
-export const liquidityPoolFactoryAddress = "";
+export const liquidityPoolFactoryAddress =
+  "0xEc51A870f5397f6eC42D92A62E87b3Bc890B0DD9";
