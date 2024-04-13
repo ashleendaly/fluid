dc83791526a033f9e81891c0e630ab2f1219f7c4
KC-tech-dotcom <keiraemmacrafts@gmail.com>
2024-04-13 14:29:29 +0100

    whiskeyToken update3

--- smart-contracts/contracts/WhiskyToken.sol
+++ smart-contracts/contracts/WhiskyToken.sol
@@ -6,21 +6,21 @@ import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract CaskTokenContract is ERC1155, Ownable {
-    unit265[] private allCasksCreated;
+    uint256[] private allCasksCreated;
     constructor(address initialOwner) ERC1155("") Ownable(initialOwner) {
 
     }
 
-    function mintNewCask(address accountTokenBeingSentTo, uint256 id, bytes memory data) public {
+    function mintNewCask(address accountTokenBeingSentTo, uint256 id) public {
         bytes memory defaultData = hex"12";
         uint256 amount = 1000;
-        _mint(accountTokenBeingSentTo, id, amount, data);
+        _mint(accountTokenBeingSentTo, id, amount, defaultData);
         // call liquidity pool factory
     }
     
     function generatePortfolio(address owner) public view returns (uint256[] memory) {
         // get the number of cask token types that this person has
-        uint265 caskTokenCount = 0;
+        uint256 caskTokenCount = 0;
 
         for (uint256 i = 0; i < allCasksCreated.length; i++) {
             if (balanceOf(owner, allCasksCreated[i]) > 0) {
@@ -39,11 +39,6 @@ contract CaskTokenContract is ERC1155, Ownable {
         }
 
         return heldCasks;
-        
-
-
-
-
          
     }
     
