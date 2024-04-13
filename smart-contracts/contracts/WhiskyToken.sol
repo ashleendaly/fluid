7ba3fd67ab4600e7fa892583af1ce52b8a27ee51
katemchugh <kate_mchugh@ymail.com>
2024-04-13 15:36:32 +0100

    liquidity pool with 0xsequence

--- smart-contracts/contracts/WhiskyToken.sol
+++ smart-contracts/contracts/WhiskyToken.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 
 contract CaskTokenContract is ERC1155, Ownable {
     uint256[] private allCasksCreated;
@@ -18,6 +19,17 @@ contract CaskTokenContract is ERC1155, Ownable {
         // call liquidity pool factory
     }
     
+    function mintBatch(address to, uint256[] memory ids, bytes memory data) public {
+        // get the size of the ids list
+        uint256 numberOfCasksinBatch = ids.length;
+        // amounts = size of the id list * 1000
+        uint256[] memory amounts = new uint256[] (numberOfCasksinBatch);
+        for (uint256 i = 0; i < numberOfCasksinBatch; i++){
+            amounts[i] = 1000;
+        }
+        _mintBatch(to, ids, amounts, data);
+        }
+
     function generatePortfolio(address owner) public view returns (uint256[] memory) {
         // get the number of cask token types that this person has
         uint256 caskTokenCount = 0;
