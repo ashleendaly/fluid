c26b0f28ea061900ca2c90d832e2050bbc63351d
KC-tech-dotcom <keiraemmacrafts@gmail.com>
2024-04-14 04:19:21 +0100

    portfolio business logic

--- dapp/src/app/portfolio/page.tsx
+++ dapp/src/app/portfolio/page.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+// import { useState, useEffect, useContext } from 'react';
+
 import {
   Table,
   TableBody,
@@ -8,40 +10,78 @@ import {
   TableRow,
 } from "@/components/ui/table";
 
-type Portfolio = {
-  caskId: number;
-  balance: number;
-};
+// creating a data type.
+// type SingleCask = {
+//   caskId: number;
+//   balance: number;
+// };
 
-const testData: Portfolio[] = [
-  { caskId: 1, balance: 100 },
-  { caskId: 2, balance: 200 },
-];
 
 const Portfolio = () => {
+
+  // const portfolio: SingleCask[] = [];
+  // const [price, setPrice] = useState(0);
+
+  // useEffect(() => {
+  //   fetchPortfolioCasks();
+  //   fetchCurrentPrice();
+  // }, []);
+
+  type Portfolio = {
+    caskId: number;
+    balance: number; // number of units owned.
+    invested: number; // number of XRP had been invested at the exchange
+  };
+
+  const testData: Portfolio[] = [
+    { caskId: 1, balance: 100, invested: 10 },
+    { caskId: 2, balance: 200, invested: 15 },
+  ];
+
+
+  // // fetching from backend AND setting portfolio variable.
+  // const fetchPortfolioCasks = async () => {
+  //   // casksHeldByID = contact.generatePorfolio;
+  //   // for (uint256 i = 0; i < casksHeldByID.length; i++) {
+  //   //   const singleCask: SingleCask = { caskId: casksHeldByID[i], balance: balanceOf(owner, casksHeldByID[i]) }
+  //   //   portfolio.push(singleCask)
+  //   // }
+  // }
+
+  const fetchCurrentPrice = async () => {
+    // TODO: make a call to the liquidity pool contract that enable us to get price through the getBuyPrice method.
+    setPrice(price);
+  }
+
+
+  // Cask is the cask ID fetched above.
+  // Price is the 
+  // Invested is the number of XRP invested to get these cask tokens : when the user invokes a the function in one of the contracts to "buy" tokens using XRP, that has to update the above with the amount of XRP
+  // Units Owned is the same "balance" in Portfolio data type above- it gets fetched.
+
   return (
-    <div className="p-5 flex flex-col gap-2">
-      <Table>
+    <div className="p-5">
+      <Table className="w-full border-collapse">
         <TableHeader>
-          <TableRow>
-            <TableHead className="w-[100px]">Asset</TableHead>
-            <TableHead>Price</TableHead>
-            <TableHead>Invested</TableHead>
-            <TableHead>Units</TableHead>
-            <TableHead className="text-right">Trade</TableHead>
+          <TableRow className="bg-gray-100">
+            <TableHead className="py-2 px-4 text-sm font-semibold text-gray-600">Cask ID ({testData.length})</TableHead>
+            <TableHead className="py-2 px-4 text-sm font-semibold text-gray-600">Price</TableHead>
+            <TableHead className="py-2 px-4 text-sm font-semibold text-gray-600">Invested (XRP)</TableHead>
+            <TableHead className="py-2 px-4 text-sm font-semibold text-gray-600">Tokens Owned</TableHead>
+            <TableHead className="py-2 px-4 text-sm font-semibold text-gray-600 text-right">Trade</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           {testData.map((cask) => {
             return (
-              <TableRow className="border p-3" key={cask.caskId}>
-                <TableCell>{cask.caskId}</TableCell>
-                <TableCell>-</TableCell>
-                <TableCell>-</TableCell>
-                <TableCell>{cask.balance}</TableCell>
-                <TableCell>
+              <TableRow className="border-b border-gray-200" key={cask.caskId}>
+                <TableCell className="py-2 px-4">{cask.caskId}</TableCell>
+                <TableCell className="py-2 px-4">-</TableCell>
+                <TableCell className="py-2 px-4">{cask.invested}</TableCell>
+                <TableCell className="py-2 px-4">{cask.balance}</TableCell>
+                <TableCell className="py-2 px-4 text-right">
                   <Link
-                    className="bg-green-400 p-1 px-2 rounded"
+                    className="text-white bg-green-500 hover:bg-green-600 py-1 px-2 rounded-md"
                     href={`/${cask.caskId}`}
                   >
                     Trade
